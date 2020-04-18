ca8f033264b295eb040b1dc4f764ae9328603538
sob05001 <solomon.berhe@gmail.com>
2020-04-18 18:10:39 +0200

    add data file filter to map

--- geocoding.py
+++ geocoding.py
@@ -2,41 +2,33 @@ from bs4 import BeautifulSoup
 import requests
 import json
 import urllib
+from datetime import date, timedelta
 
-filePath = "Covid-19.csv" #original
-filePathWithGeo = "Covid-19-geo.csv" #with geo data
-filePathGeocodesOnly = "geocodes.txt" #Geocodes
+today = date.today().strftime("%Y-%m-%d")
+yesterday = (date.today() - timedelta(days=5)).strftime("%Y-%m-%d")
+print(today,yesterday)
+
+geocodingFile = "geocodes.txt" #Geocodes
 geocodeUrl = "https://maps.googleapis.com/maps/api/geocode/json?key=SECRETKEY&address="
-dataSrcBaseUrls = "https://github.com/MinCiencia/Datos-COVID19/tree/master/output/producto4"
+remoteCsvFile = "https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto4/"+yesterday+"-CasosConfirmados-totalRegional.csv"
+localCsvFile = yesterday+"-CasosConfirmados-totalRegional.csv"
+localCsvGeoFile = yesterday+"-CasosConfirmados-totalRegional-geo.csv"
 
-def getLatestDataSrcUrl():
-	req = requests.get(dataSrcBaseUrls, requests.utils.default_headers())
-	soup = BeautifulSoup(req.content, 'html.parser')
-	dataFileUrls = []
-	dataFileUrlLatest = ""
-	for a in soup.find_all('a', href=True):
-		if a['href'].endswith(".csv"):
-			dataFileUrls.append(str(a['href']))
-	dataFileUrlLatest = "https://raw.githubusercontent.com" + dataFileUrls[len(dataFileUrls)-1].replace("/blob","")
-	print("Found the latest data file:", dataFileUrlLatest)
-	return dataFileUrlLatest
 
-def getLatestDataSrcFileByUrl():
-	urllib.request.urlretrieve(getLatestDataSrcUrl(),"Covid-19.csv")
-
+urllib.request.urlretrieve(remoteCsvFile,localCsvFile)
 
 counter = 0;
-with open(filePath, "r") as f:
+with open(localCsvFile, "r") as f:
 	next(f) 
 	for line in f:
-		print("Comment break function due to expensive call")
-		break
+		#print("Comment break function due to expensive call")
+		#break
 
 		counter = counter + 1
 		lineArr = [x.strip() for x in line.split(',')] # convert line string to array
 		address = lineArr[0] # extract address info
 
-		r = requests.get(geocodeUrl+address) # get geocoding data by address
+		r = requests.get(geocodeUrl+address+" Chile") # get geocoding data by address and country 
 		data = json.loads(r.content) # convert string to json
 
 		if len(data["results"]) == 0:
@@ -48,8 +40,8 @@ with open(filePath, "r") as f:
 		lat = data["results"][0]["geometry"]["location"]["lat"]
 		lng = data["results"][0]["geometry"]["location"]["lng"]
 
-		with open(filePathWithGeo, "a") as myfile:
+		with open(localCsvGeoFile, "a") as myfile:
 			myfile.write(str(lat)+","+str(lng)+","+line)
 
-		with open(filePathGeocodesOnly, 'a') as the_file:
+		with open(geocodingFile, 'a') as the_file:
 			the_file.write(str(data)+"\n")
